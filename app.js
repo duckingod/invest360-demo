5fcd7a6b993e7e0a208cdca8a99c83394e3e8d17
duckingod <kingoduck@gmail.com>
2023-04-22 21:14:28 +0800

    update design

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-const userInput = document.getElementById("user-input");
-const submitBtn = document.getElementById("submit-btn");
-const scorePanel = document.querySelector(".score-panel");
-const feedbackPanel = document.querySelector(".feedback-panel");
-const score = document.getElementById("score");
-const feedbackList = document.getElementById("feedback-list");
+const userInput = document.getElementById('user-input');
+const submitBtn = document.getElementById('submit-btn');
+const outputPanel = document.querySelector('.output-panel');
+const score = document.getElementById('score');
+const feedbackList = document.getElementById('feedback-list');
 
-submitBtn.addEventListener("click", function() {
-  if (userInput.value.trim() === "") {
-    alert("Please enter your opinion");
-    return;
+submitBtn.addEventListener('click', () => {
+  const userOpinion = userInput.value.trim();
+  
+  if (userOpinion !== '') {
+    const analysisResult = analyzeOpinion(userOpinion);
+    score.textContent = analysisResult.score;
+    renderFeedback(analysisResult.feedback);
+    outputPanel.style.display = 'flex';
   }
-
-  // simulate a score calculation
-  const randomScore = Math.floor(Math.random() * 100);
-  score.textContent = randomScore;
-
-  // simulate a feedback list
-  const feedbackItems = [
-    "Your opinion is well-reasoned.",
-    "You could provide more evidence to support your opinion.",
-    "Your opinion is too focused on short-term gains.",
-  ];
-
-  feedbackList.innerHTML = feedbackItems.map(item => `<li>${item}</li>`).join("");
-
-  // slide in the score panel and feedback panel
-  scorePanel.style.display = "block";
-  feedbackPanel.style.display = "block";
 });
+
+function analyzeOpinion(opinion) {
+  const result = { score: 53, feedback: [
+    'some feedback'
+  ] };
+  
+  // Add your analysis logic here
+  
+  return result;
+}
+
+function renderFeedback(feedback) {
+  feedbackList.innerHTML = '';
+  
+  feedback.forEach(item => {
+    const li = document.createElement('li');
+    li.innerHTML = `${item.label}: <span>${item.text}</span>`;
+    feedbackList.appendChild(li);
+  });
+}
